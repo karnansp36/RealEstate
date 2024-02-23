2ae189e27f19a205ef3765eafd442f43969b0104
karnan_sp <reviewmaster36@gmail.com>
2024-02-23 11:03:51 +0530

    middleware for error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup =async(req, res) => {
+export const signup =async(req, res, next) => {
     const {username , email, password}= req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username : username, email:email, password:hashedPassword});
@@ -11,7 +11,7 @@ export const signup =async(req, res) => {
       res.status(201).json( 'User signed up successfully');
     }
     catch(error){
-      res.status(500).json(error.message);
+      next(error);
     }
     
   }
--- api/index.js
+++ api/index.js
@@ -18,6 +18,16 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
 
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        sucess:false,
+        statusCode,
+        message
+    });
+});
+
 app.listen(3000, ()=>{
     console.log('server is running in port 3000');
 })
