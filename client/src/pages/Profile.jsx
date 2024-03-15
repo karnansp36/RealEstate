e4ad0de958bd80e3cfc169262f6994849c54954d
karnan_sp <reviewmaster36@gmail.com>
2024-03-15 15:26:35 +0530

    complete profile ui page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 export default function Profile() {
+    const {currentUser}  = useSelector((state) => state.user);
   return (
-    <div>
-      Profile Page
+    <div className="profile-container">
+      <h1>Profile</h1>
+      <div className="profilePage-img">
+        <img src={currentUser.avatar} className="avatar-img" alt="profile-image" />
+      </div>
+      
+        <div className='profile-contain'>
+        
+        <form  className='profile-wrapper'>
+          <input type="text" name="username" placeholder='username' id="username" className='profile-i' />
+          <input type="email" name="email" placeholder='email' id="email" className='profile-i' />
+          <input type="password" name="password" placeholder='password' id="password" className='profile-i'  />
+          <button className="update-btn">Update</button>
+          <div className="link-container">
+            <span className="delete-link">Delete Account</span>
+            <span className="signout-link">Signout</span>
+          </div>
+        </form>
+          
+         
+        
+        </div>
+      
+      
     </div>
   )
 }
