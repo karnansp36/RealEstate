59df19502a1cccf3988aa54de975c8b7e7fd8838
karnan_sp <reviewmaster36@gmail.com>
2024-03-16 15:16:14 +0530

    created user update api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -13,7 +14,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
+
+// routes middleware
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
